f5619346bdcd564f0ee80d8ffdead30d575e7d2e
pouyaesm <pouya.esmailian@gmail.com>
2018-07-14 04:58:30 +0100

    deprication levels added

---
+++ src/main/kotlin/tutorial/Deprecation.kt
@@ -0,0 +1,23 @@
+package tutorial
+
+fun main(args: Array<String>){
+  // Not found (but compilable)
+  noMoreUse()
+  // Raises error
+  seeButNoMoreUse()
+  // Raises IDE warning
+  betterNotToUse()
+}
+
+@Deprecated("Use useThis()", level = DeprecationLevel.HIDDEN)
+fun noMoreUse() = println("Function is usable, but not visible to IDE any more")
+
+@Deprecated("Use useThis()", level = DeprecationLevel.ERROR,
+        replaceWith = ReplaceWith("useThis()"))
+fun seeButNoMoreUse() = println("Usage raises error")
+
+@Deprecated("Use useThis().", level = DeprecationLevel.WARNING,
+        replaceWith = ReplaceWith("useThis()"))
+fun betterNotToUse() = println("Better not to use this function")
+
+fun useThis() = println("Use this")
