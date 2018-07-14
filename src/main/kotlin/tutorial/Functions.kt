9cdd8235d190f7e2047de70e7c71d1973196d88c
pouyaesm <pouya.esmailian@gmail.com>
2018-07-14 23:39:43 +0100

    generic class access added

--- src/main/kotlin/tutorial/Functions.kt
+++ src/main/kotlin/tutorial/Functions.kt
@@ -120,8 +120,8 @@ inline fun execute(input: Any, function: (Any) -> Any) {
  */
 inline fun <reified T> genericFunction(x: Any){
   if(x is T){
-    println("'$x' type matches the generic type")
+    println("'$x' type matches the generic type ${T::class}")
   }else{
-    println("'$x' type doesn't match the generic type")
+    println("'$x' type doesn't match the generic ${T::class}")
   }
 }
